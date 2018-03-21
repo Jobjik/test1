ff0a82348bf8589cedd878d0b46b51c25bf32c4a
Jobjik <cyber_hedgehog@mail.ru>
2018-03-21 22:47:10 +0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/pc.jpg') {
+      myImage.setAttribute ('src','images/pc1.jpg');
+    } else {
+      myImage.setAttribute ('src','images/pc.jpg');
+    }
+  }
+
+
+  var myButton = document.querySelector('button');
+  var myHeading = document.querySelector('h1');
+  function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML  = 'Магазин кампутеров, ' + myName;
+}
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML  = 'Магазин кампутеров, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+  setUserName();
+}
